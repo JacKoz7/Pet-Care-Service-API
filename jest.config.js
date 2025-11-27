3136810b3be67b25f5ad09023158a9940baf51ee
Jacek <jacuskozlowski@gmail.com>
2025-11-27 17:13:14 +0100

    test: add jest setup and tests for user attributes endpoint

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "node",
+  moduleNameMapping: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.tsx"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/_*.{js,jsx,ts,tsx}",
+  ],
+};
+
+export default createJestConfig(customJestConfig);
